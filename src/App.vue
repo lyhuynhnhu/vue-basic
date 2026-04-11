c73b51fa0a9aceb6b10b7ff8a9574e3d7bca501d
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-11 17:17:08 +0700

    template syntax

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,38 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+const message = 'Hello World!'
+const rawHtml = '<span style="color: red">This should be red</span>'
+const dynamicId = 'dynamic-id'
+const isButtonDisabled = true
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: {
+    color: 'green',
+  },
+}
+const number = 2
+const reversedMessage = (msg) => msg.split('').reverse().join('-')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>Template syntax</h1>
+  <p>Using text interpolation: {{ message }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div v-bind:id="dynamicId">Attribute binding</div>
+  <div :id="dynamicId">Attribute binding - shorthand</div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <button :disabled="isButtonDisabled">Button</button>
 
-  <RouterView />
+  <div v-bind="objectOfAttrs">Binding multi attributes</div>
+
+  <p>js expression: 1 + 1 = {{ 1 + 1 }}</p>
+
+  <div>{{ number + 1 }}</div>
+
+  <button :disabled="isButtonDisabled">{{ isButtonDisabled ? 'Disabled' : 'Enabled' }}</button>
+
+  <div :id="`msg-${dynamicId}`">{{ message.split('').reverse().join('') }}</div>
+
+  <div :id="reversedMessage(dynamicId)">{{ reversedMessage(message) }}</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
