32828b13ffa1a9cb5b995750c41e2125a793928d
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-13 21:15:48 +0700

    biding class

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,67 @@
 <script setup>
-import { reactive, computed, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
-const author = reactive({
-  name: 'John Doe',
-  books: ['Vue 2 - Advanced Guide', 'Vue 3 - Basic Guide', 'Vue 4 - The Mystery'],
-  published: true,
+const isActive = ref(false)
+const hasError = ref(true)
+
+const classObj = reactive({
+  active: true,
+  'has-error': false,
 })
 
-const unpublish = () => {
-  author.published = false
-}
+const comptedClass = computed(() => ({
+  active: isActive.value && !hasError.value,
+  'has-error': hasError.value,
+}))
 
-// a computed ref - only recompute when author.published changes
-// value is read-only
-// -> prevent unnecessary computations
-const isPublished = computed(() => {
-  return author.published ? 'Yes' : 'No'
-})
-
-const firstName = ref('Nguyen Van')
-const lastName = ref('A')
-const fullName = computed({
-  get() {
-    return `${firstName.value} ${lastName.value}`
-  },
-  set(newValue) {
-    ;[firstName.value, lastName.value] = newValue.split(',')
-  },
-})
-
-const changeFullName = () => {
-  fullName.value = 'Tran Van,B'
-}
+const changeStatus = () => (hasError.value = !hasError.value)
 </script>
 
 <template>
-  <h1>computed properties</h1>
-  <p>Author {{ author.name }} has published {{ author.books.length }} books.</p>
-  <p>Published: {{ isPublished }}</p>
-  <button @click="unpublish">Unpublish</button>
+  <h3>Binding class</h3>
 
-  <h4>* Writable computed</h4>
-  <p>First name: {{ firstName }}</p>
-  <p>Last name: {{ lastName }}</p>
-  <p>Full name: {{ fullName }}</p>
-  <button @click="changeFullName">Change fullname</button>
+  <button class="button" :class="{ active: isActive, 'has-error': hasError }">
+    {{ hasError ? 'Error' : 'Normal' }}
+  </button>
+  <br /><br />
+  <button class="button" :class="classObj">Reactive class</button>
+  <br /><br />
+  <button class="button" :class="comptedClass">Computed class</button>
+  <br /><br />
+  <button
+    @click="changeStatus"
+    class="button"
+    :class="[isActive ? 'active' : '', { 'has-error': hasError }]"
+  >
+    Binding array
+  </button>
 </template>
+
+<style>
+.button {
+  border: 1px solid green;
+  background: #f5f5f5;
+  color: green;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.active {
+  border: none;
+  background: green;
+  color: #fff;
+  &:hover {
+    background: darkgreen;
+  }
+}
+
+.has-error {
+  border: none;
+  background: rgb(229, 57, 57);
+  color: #fff;
+  &:hover {
+    background: rgb(196, 31, 31);
+  }
+}
+</style>
