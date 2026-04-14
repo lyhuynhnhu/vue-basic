75086bf2e721d8ed0957d0afc8c869ad2a738a84
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-14 22:21:25 +0700

    v-for: loop for object

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,49 @@ const fruits = ref([
     id: 1,
     name: 'apple',
     amount: 5,
+    isStocked: true,
   },
   {
     id: 2,
     name: 'banana',
     amount: 10,
+    isStocked: false,
   },
   {
     id: 3,
     name: 'orange',
     amount: 15,
+    isStocked: true,
   },
 ])
+
+const user = ref({
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 30,
+})
 </script>
 
 <template>
-  <h3>v-for</h3>
+  <h3>v-for for array</h3>
   <ul>
-    <li v-for="fruit in fruits" :key="fruit.id">{{ fruit.name }} - {{ fruit.amount }}</li>
+    <li v-for="(fruit, index) in fruits" :key="fruit.id">
+      {{ index }}. {{ fruit.name }} - {{ fruit.amount }}
+    </li>
   </ul>
   <span v-for="n in 10" :key="n">{{ n }}</span>
-</template>
 
-<style>
-.button {
-  border: 1px solid green;
-  background: #f5f5f5;
-  color: green;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-}
-</style>
+  <h3>v-for for object</h3>
+  <ul>
+    <li v-for="(value, key) in user" :key="key">{{ key }}: {{ value }}</li>
+  </ul>
+
+  <p>Fruits in stock:</p>
+  <ul>
+    <template v-for="(fruit, index) in fruits">
+      <li v-if="fruit.isStocked" :key="fruit.id">
+        {{ fruit.name }} - {{ fruit.amount }} (index: {{ index }})
+      </li>
+    </template>
+  </ul>
+</template>
