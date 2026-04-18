4f8044a6937612368a08fd7ad262d17e24f58384
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-18 20:24:10 +0700

    watchers

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-## Conditional rendering
+\*\*\* Watchers là 1 tính năng cho phép "theo dõi" sự thay đổi của 1 biến (data hoặc computed) và thực hiện 1 hành động nào đó (side effect) khi giá trị của biến đó thay đổi
 
-- v-if
-  - thêm hoặc xoá hẳn phần tử trong DOM
-  - là điều kiện để render phần tử
-  - có thể dùng cho `<template>`
-  - hiệu suất render ban đầu nhanh hơn v-show (trhop condition là false -> nó ko cần làm gì cả)
-  - hiệu suất khi chuyển chuyển đổi lại chậm hơn (vì phải xoá/tạo ptu)
-  - dùng khi điều kiện hiển thị ít khi thay đổi trong suốt vòng đời của page
+\*\* Khi nào nên dùng Watchers?
 
-- v-show
-  - ẩn hoặc hiển thị phần tử trên DOM
-  - phẩn tử luôn tồn tại trong DOM
-  - chỉ thay đổi thuộc tính CSS `display`
-  - not support for `<template>`
-  - hiệu suất render ban đầu sẽ chậm hơn v-if
-  - hiệu suất khi chuyển đổi nhanh hơn nhiều (do chỉ bật tắt css)
-  - dùng khi cần bật/tắt liên tục (ví dụ: menu dropdown, các tab, hoặc thanh tìm kiếm)
+=> khi cần thực hiện các tác vụ không đồng bộ hoặc các logic phức tạp sau khi dữ liệu thay đổi:
+
+- Gọi API khi một giá trị đầu vào thay đổi (ví dụ: tìm kiếm sản phẩm khi người dùng nhập tên)
+
+- Thao tác với DOM thủ công
+
+- Lưu dữ liệu vào localStorage
+
+- Thiết lập bộ đếm thời gian (Timers)
--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,30 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const isLoggedIn = ref(false)
+const question = ref('')
+const answer = ref('Questions usually contain a question mark')
+const isLoading = ref(false)
 
-const toggle = () => (isLoggedIn.value = !isLoggedIn.value)
-
-const status = ref('online')
-
-const changeStatus = () => {
-  if (status.value === 'online') {
-    status.value = 'busy'
-  } else if (status.value === 'busy') {
-    status.value = 'offline'
-  } else if (status.value === 'offline') {
-    status.value = 'online'
-  } else {
-    status.value = 'unknown'
+watch(question, async (newQuestion, oldQuestion) => {
+  if (newQuestion.includes('?')) {
+    isLoading.value = true
+    answer.value = 'Thinking...'
+    try {
+      const res = await fetch('https://yesno.wtf/api')
+      answer.value = (await res.json()).answer
+    } catch (error) {
+      answer.value = 'Error! Could not reach the API. ' + error
+    } finally {
+      isLoading.value = false
+    }
   }
-}
-
-const isShow = ref(true)
-
-const toggleShow = () => (isShow.value = !isShow.value)
+})
 </script>
 
 <template>
-  <h3>v-if</h3>
-
-  <p v-if="isLoggedIn">User is logged in</p>
-  <p v-else>User is not logged in</p>
-  <button @click="toggle" class="button">{{ isLoggedIn ? 'Logout' : 'Login' }}</button>
-
-  <p v-if="status === 'online'">User is online</p>
-  <p v-else-if="status === 'busy'">User is busy</p>
-  <p v-else-if="status === 'offline'">User is offline</p>
-  <p v-else>User status is unknown</p>
-  <button @click="changeStatus" class="button">Change status</button>
-
-  <h3>v-show</h3>
-  <p v-show="isShow">This content is visible</p>
-  <button @click="toggleShow" class="button">{{ isShow ? 'Hide' : 'Show' }}</button>
+  <p>
+    Ask a yes/no question:
+    <input v-model="question" :disabled="isLoading" />
+  </p>
+  <p>{{ answer }}</p>
 </template>
-
-<style>
-.button {
-  border: 1px solid green;
-  background: #f5f5f5;
-  color: green;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-}
-</style>
