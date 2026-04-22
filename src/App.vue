6fdcc010228be08f3d060b933d8558f32253e621
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-22 20:35:43 +0700

    props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import ButtonCount from './components/ButtonCount.vue'
 </script>
 
 <template>
@@ -8,7 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hello world!" />
+      <ButtonCount :init-count="1" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  initCount: Number,
+})
+const count = ref(props.initCount || 0)
+const countPlus = computed(() => props.initCount + 1)
+</script>
+
+<template>
+  <button type="button" @click="count++">{{ count }}</button>
+  <p>init count plus: {{ countPlus }}</p>
+</template>
