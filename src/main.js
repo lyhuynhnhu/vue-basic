3b0c657f97625a7651dbe7b53c0971b50649083c
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-23 21:18:12 +0700

    subcribed global component

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import TheWelcome from './components/TheWelcome.vue'
 
 const app = createApp(App)
+
+app.component('GlobalCompA', TheWelcome).component('GlobalCompB', TheWelcome)
 
 app.use(router)
 
