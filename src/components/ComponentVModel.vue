bcfcdb4b0dc5ec44f9cad9b411ae1f6bf27071cc
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-23 21:40:40 +0700

    v-model: arguments & multiple bindings

--- src/components/ComponentVModel.vue
+++ src/components/ComponentVModel.vue
@@ -4,12 +4,22 @@
   <input type="text" v-model="name" />
   <p>Name: {{ name }}</p>
   <button @click="changeName">Change name</button>
+
+  <p>Address</p>
+  <input type="text" v-model="address" />
+  <p>Address: {{ address }}</p>
+  <button @click="changeAddress">Change address</button>
 </template>
 
 <script setup>
-const name = defineModel()
+const name = defineModel('name', { type: String })
+const address = defineModel('address', { type: String })
 
 const changeName = () => {
   name.value = 'John Doe'
+}
+
+const changeAddress = () => {
+  address.value = 'New York'
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,14 @@ import ComponentVModel from '@/components/ComponentVModel.vue'
 import { ref, watchEffect } from 'vue'
 
 const name = ref('')
+const address = ref('')
 
 const setDefaultName = () => {
   name.value = 'Huynh Nhu'
+}
+
+const setDefaultAddress = () => {
+  address.value = 'Ho Chi Minh'
 }
 
 watchEffect(() => {
@@ -16,7 +21,9 @@ watchEffect(() => {
 <template>
   <main>
     <GlobalCompA />
-    <ComponentVModel v-model="name" />
+    <ComponentVModel v-model:name="name" v-model:address="address" />
+    <br />
     <button @click="setDefaultName">Set default name</button>
+    <button @click="setDefaultAddress">Set default address</button>
   </main>
 </template>
