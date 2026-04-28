0a955865f62d5f4fa4f5a0d9f74e101764f7f59b
Nhu Ly <lyhuynhnhu200699@gmail.com>
2026-04-28 14:39:36 +0700

    async component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,20 @@
 import { inject } from 'vue'
 
 const { locale } = inject('locale')
+
+// Async component
+// const AsyncComp = defineAsyncComponent({
+//   // Hàm import component
+//   loader: () => import('./components/MyHeavyComponent.vue'),
+
+//   // Component hiện ra trong khi đang tải
+//   loadingComponent: LoadingSpinner,
+//   delay: 200, // Độ trễ trước khi hiện loading (ms)
+
+//   // Component hiện ra nếu tải thất bại
+//   errorComponent: ErrorComponent,
+//   timeout: 3000 // Hủy nếu quá thời gian này (ms)
+// })
 </script>
 
 <template>
